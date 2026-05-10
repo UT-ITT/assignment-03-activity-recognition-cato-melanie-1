41a4b49029b213a8ea0633465fdfbd31c388e1d7
Cato <cato.kurtz@student.uni-tuebingen.de>
2026-05-10 15:22:34 +0200

    added TODOs

--- fitness_trainer.py
+++ fitness_trainer.py
@@ -2,3 +2,20 @@
 
 import activity_recognizer as activity
 import pyglet
+
+#TODO
+#read training data from csv files
+#pre-process training data
+#split into train and test dataset
+#train ML classifier
+#evaluate accuracy using test dataset
+
+#start pyglet fitness app
+#(takes data and displays to the user which activity is being done and if it
+# is done correctly?)
+
+#document how to use the app in README
+
+#note:
+#put all functions that are not related to visualization in activity_recognizer
+#and just call them here
