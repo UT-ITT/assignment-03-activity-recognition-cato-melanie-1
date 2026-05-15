0748f6c62f5a569361cd698c5058628b723baca7
mholfort <mholfort@gmail.com>
2026-05-15 11:56:28 +0200

    add missing imports

--- activity_recognizer.py
+++ activity_recognizer.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn import svm
 from sklearn.preprocessing import scale, StandardScaler
+from sklearn.model_selection import train_test_split
 import os
 import glob
 
