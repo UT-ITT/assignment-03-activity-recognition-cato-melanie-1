cb167afd9a1ec05cec05d251cc952358ec15eb3b
mholfort <mholfort@gmail.com>
2026-05-16 20:45:36 +0200

    rough outline

--- fitness_trainer.py
+++ fitness_trainer.py
@@ -1,18 +1,12 @@
-# this program visualizes activities with pyglet
-
-import activity_recognizer as activity
-import pyglet
-
 #DONE
 #read training data from csv files
 #pre-process training data
 #split into train and test dataset
 #train ML classifier
 #evaluate accuracy using test dataset
-
-#TODO
 #try more preprocessing to get higher accuracy
 
+#TODO
 #design pyglet fitness app
 #take sensor data from dippid and predict which activity is being done
 #also predict if it is done correctly?
@@ -22,3 +16,47 @@ import pyglet
 #note:
 #put all functions that are not related to visualization in activity_recognizer
 #and just call them here
+
+
+# this program visualizes activities with pyglet
+from activity_recognizer import ActivityRecognizer
+from DIPPID import SensorUDP
+import pyglet
+from pyglet import window
+import os
+
+PORT = 5700
+sensor = SensorUDP(PORT)
+
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+
+win = window.Window(WINDOW_WIDTH, WINDOW_HEIGHT)
+win.set_caption("Fitness Trainer")
+
+recognizer = ActivityRecognizer()
+recognizer.train()
+
+
+def update(dt):
+    #todo
+    #predict continuously
+    #save last 200 values - 2s window
+
+    #read sensor values
+    acc_x = sensor.get_value('accelerometer')['x']
+    acc_y = sensor.get_value('accelerometer')['y']
+    acc_z = sensor.get_value('accelerometer')['z']
+    gyro_x = sensor.get_value('gyroscope')['x']
+    gyro_y = sensor.get_value('gyroscope')['y']
+    gyro_z = sensor.get_value('gyroscope')['z']
+
+
+@win.event
+def on_key_press(symbol, modifiers):
+    if symbol == pyglet.window.key.Q:
+        os._exit(0)
+
+
+pyglet.clock.schedule_interval(update, 0.01)
+pyglet.app.run()
