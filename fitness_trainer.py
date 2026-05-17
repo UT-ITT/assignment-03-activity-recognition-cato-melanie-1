afd752d5739a1432f48d74894fddae219c52ac56
Cato Kurtz <cato.kurtz@student.uni-tuebingen.de>
2026-05-17 13:26:13 +0200

    implemented activity recognition + small updates
    - updated TODOs
    - now import everything from activity recognizer, including libraries
    - changed order so window gets displayed only after training, added print statements for waiting time
    - added if and exception to catch case no data transmitted from DIPPID

--- fitness_trainer.py
+++ fitness_trainer.py
@@ -5,21 +5,19 @@
 #train ML classifier
 #evaluate accuracy using test dataset
 #try more preprocessing to get higher accuracy
+#take sensor data from dippid and predict which activity is being done
 
 #TODO
 #design pyglet fitness app
-#take sensor data from dippid and predict which activity is being done
+    #1st screen: display all activities, explain how it works
+    #activity recognition starts after pressing button? (otherwise 1st screen is gone too fast)
 #also predict if it is done correctly?
 
-#document how our training data is structured in README
-
-#note:
-#put all functions that are not related to visualization in activity_recognizer
-#and just call them here
+#document how to start the fitness App in README
 
 
 # this program visualizes activities with pyglet
-from activity_recognizer import ActivityRecognizer
+from activity_recognizer import *
 from DIPPID import SensorUDP
 import pyglet
 from pyglet import window
@@ -31,25 +29,57 @@ sensor = SensorUDP(PORT)
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
 
-win = window.Window(WINDOW_WIDTH, WINDOW_HEIGHT)
-win.set_caption("Fitness Trainer")
-
 recognizer = ActivityRecognizer()
+print("starting up activity recognizer...")
 recognizer.train()
 
+win = window.Window(WINDOW_WIDTH, WINDOW_HEIGHT)
+win.set_caption("Fitness Trainer")
+print("fitness app ready")
+
+input = {
+    "acc_x": [],
+    "acc_y": [],
+    "acc_z": [],
+    "gyro_x": [],
+    "gyro_y": [],
+    "gyro_z": []
+}
+input_df = pd.DataFrame(input)
 
 def update(dt):
-    #todo
-    #predict continuously
-    #save last 200 values - 2s window
 
-    #read sensor values
-    acc_x = sensor.get_value('accelerometer')['x']
-    acc_y = sensor.get_value('accelerometer')['y']
-    acc_z = sensor.get_value('accelerometer')['z']
-    gyro_x = sensor.get_value('gyroscope')['x']
-    gyro_y = sensor.get_value('gyroscope')['y']
-    gyro_z = sensor.get_value('gyroscope')['z']
+    global input_df
+    size = len(input_df)
+
+    #read sensor data if received
+    if not sensor.get_capabilities() == []:
+        acc_x = sensor.get_value('accelerometer')['x']
+        acc_y = sensor.get_value('accelerometer')['y']
+        acc_z = sensor.get_value('accelerometer')['z']
+        gyro_x = sensor.get_value('gyroscope')['x']
+        gyro_y = sensor.get_value('gyroscope')['y']
+        gyro_z = sensor.get_value('gyroscope')['z']
+
+        #fill 2s window with input data
+        if size < 200:
+            input_df.loc[size] = [acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z]
+        #predict recognized movement every two seconds
+        else: #size = 200
+            print(recognizer.predict(input_df)) #FIXME use prediction in fitness app
+            input = {
+                "acc_x": [],
+                "acc_y": [],
+                "acc_z": [],
+                "gyro_x": [],
+                "gyro_y": [],
+                "gyro_z": []
+            }
+            input_df = pd.DataFrame(input)
+
+    else:
+        raise Exception("Sorry, there seems to be a problem with the data input. Did you start the DIPPID app and set it to the correct IP and Port?")
+        
 
 
 @win.event
